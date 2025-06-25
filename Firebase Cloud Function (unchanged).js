d02352b306fc2a88f5b2717bf6c1bc4702282c7d
NTRTech1856 <james.whitehead@newtheatreroyal.com>
2025-06-25 18:02:41 +0100

    Create  Firebase Cloud Function (unchanged).js

---
+++ Firebase Cloud Function (unchanged).js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.clearSignedInUsersDaily = functions.pubsub
+  .schedule("0 4 * * *") // 4 AM daily UTC, change timezone below
+  .timeZone("YOUR_TIMEZONE") // e.g. "America/New_York"
+  .onRun(async () => {
+    const snapshot = await db.collection("signedInUsers").get();
+    const batch = db.batch();
+
+    snapshot.docs.forEach((doc) => batch.delete(doc.ref));
+    await batch.commit();
+
+    console.log("Cleared signedInUsers collection at 4 AM");
+    return null;
+  });
